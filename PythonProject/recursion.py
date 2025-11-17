40413d1395b206b2ef3112fd0ef98175df37c89f
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2025-11-17 09:52:23 +0530

    add the recursion

---
+++ PythonProject/recursion.py
@@ -0,0 +1,23 @@
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+    
+# print(factorial(5))
+
+# def fibonacci(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)
+# print(fibonacci(10))
+
+# def fibonacci(n):
+#     a,b = 0,1
+#     for _ in range(n):
+#         print(a, end=" ")
+#         a,b = b, a + b
+# fibonacci(10)
